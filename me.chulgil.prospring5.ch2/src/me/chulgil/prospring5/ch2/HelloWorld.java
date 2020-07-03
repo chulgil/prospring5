963baf84927b3d014c6fc94939a019201aafdcc3
CG.Lee <contact@chulgil.me>
2020-07-03 16:38:04 +0900

    Init spring project

---
+++ me.chulgil.prospring5.ch2/src/me/chulgil/prospring5/ch2/HelloWorld.java
@@ -0,0 +1,16 @@
+package me.chulgil.prospring5.ch2;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			System.out.println(args[0]);
+		} else {
+			System.out.println("Hello World!");
+		}
+			
+
+	}
+
+}
